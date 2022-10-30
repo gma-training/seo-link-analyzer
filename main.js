535328120640073df9833530b2544ae98a71650c
Graham Ashton <graham@effectif.com>
2022-10-30 21:21:20 +0000

    main.js expects to be called with base URL

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const path = require("node:path");
+
+function usage() {
+  const basename = path.basename(process.argv[1]);
+  console.error(`Usage: node ${basename} <base-url>`);
+  process.exit(1);
+}
+
+function main() {
+  const args = process.argv.slice(2);
+  args.length === 1 || usage();
+}
+
+main();
