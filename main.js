608f198e7839463d0dd71bb2ed7532c9d02a38ae
Graham Ashton <graham@effectif.com>
2022-11-02 17:29:37 +0000

    Update main.js to call crawlPage()
    Run it like this:
    
        $ node main.js https://effectif.com

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 const path = require("node:path");
 
+const { crawlPage } = require("./crawl");
+
 function usage() {
   const basename = path.basename(process.argv[1]);
   console.error(`Usage: node ${basename} <base-url>`);
   process.exit(1);
 }
 
-function main() {
+function onError(message) {
+  console.error(`Skipping ${message}`);
+}
+
+function printReport(pages) {
+  const results = Array.from(pages.entries()).sort((a, b) =>
+    a[1] > b[1] ? 1 : -1
+  );
+
+  for (const [link, count] of results) {
+    const countColumn = Number(count).toString().padStart(8);
+    console.log(`${countColumn} ${link}`);
+  }
+}
+
+async function main() {
   const args = process.argv.slice(2);
   args.length === 1 || usage();
+  const url = args[0];
+
+  pages = await crawlPage(url, url, { onError });
+
+  printReport(pages);
 }
 
 main();
